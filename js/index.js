8df42002bb576a655ba82d3ab3dde8bf2b17ffe8
DaniKard <kajothada@gmail.com>
2020-10-06 12:28:01 +0200

    add toggle function

--- js/index.js
+++ js/index.js
@@ -6,4 +6,21 @@
     $("#p2",).click (function (){
         $(".ii2").toggle(),
         $(".pp2").toggle();
-         
+
+        $("#i1").click (function (){
+            $(".pp1").toggle(),
+            $(".ii1").toggle();  
+          });
+          $("#p1").click (function (){
+            $(".ii1").toggle(),
+            $(".pp1").toggle();
+           });
+           $("#i3").click (function (){
+            $(".pp3").toggle(),
+            $(".ii3").toggle();  
+          });
+          $("#p3").click (function (){
+            $(".ii3").toggle(),
+            $(".pp3").toggle();
+           });
+            
