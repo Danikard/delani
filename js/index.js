38bfd9499024150c9a1884e0ab6ad7511bd024d5
DaniKard <kajothada@gmail.com>
2020-10-06 11:15:57 +0200

    add functions in js

---
+++ js/index.js
@@ -0,0 +1,9 @@
+(document).ready(function(){ 
+    $("#i2").click (function (){
+      $(".pp2").toggle(),
+      $(".ii2").toggle();  
+    });
+    $("#p2",).click (function (){
+        $(".ii2").toggle(),
+        $(".pp2").toggle();
+         
